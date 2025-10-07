b6125f71c99326c0578ed1df885f6b3816dfd4d0
BlackboxAI <blackboxai@example.com>
2025-10-07 10:29:43 +0530

    Fix syntax error in GlassDashboard.js by reordering function definitions before useEffect hooks

--- client/src/components/GlassDashboard.js
+++ client/src/components/GlassDashboard.js
@@ -13,23 +13,6 @@ const GlassDashboard = ({ DashUser, setDashUser }) => {
   const [render, setRender] = useState();
   const [chartType, setChartType] = useState('line');
 
-
-
-  useEffect(() => {
-    fetchData();
-    if (DashUser.length === undefined) {
-      let localUser = JSON.parse(localStorage.getItem('user'));
-      // console.log("local", localUser);
-      setDashUser(localUser);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  useEffect(() => {
-    chartRender();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chartType])
-
   const fetchData = async () => {
     let resp = await getUsers();
     // console.log("resp dash", resp)
@@ -47,7 +30,22 @@ const GlassDashboard = ({ DashUser, setDashUser }) => {
     let result = graphs.find(item => item.type === chartType);
     // console.log('result', result.component);
     setRender(result.component);
-  }
+  };
+
+  useEffect(() => {
+    fetchData();
+    if (DashUser.length === undefined) {
+      let localUser = JSON.parse(localStorage.getItem('user'));
+      // console.log("local", localUser);
+      setDashUser(localUser);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    chartRender();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chartType])
 
   return (
     <section>
@@ -56,7 +54,7 @@ const GlassDashboard = ({ DashUser, setDashUser }) => {
           <a href='' className='logo'>Profile</a>
           <ul className='nav'>
             <li><a href='' className='active'>Home</a></li>
-            <li><a href='https://github.com/ShamsQamar?tab=repositories'>Projects</a></li>
+            <li><a href='https://github.com/ARAVIND2457-Q?tab=repositories'>Projects</a></li>
             <li>
               <select className='select' onChange={(e) => setChartType(e.target.value)}>
                 <option className='option' value='line'>line</option>
@@ -72,7 +70,7 @@ const GlassDashboard = ({ DashUser, setDashUser }) => {
         <div className='content'>
           <h2>I'm {DashUser.name}</h2>
           <p>{DashUser.role}</p>
-          <a href='https://www.linkedin.com/in/shams-qamar-703a21212?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' target='blank'>About Me</a>
+          <a href='https://github.com/dashboard' target='blank'>About Me</a>
         </div>
 
 
@@ -81,8 +79,8 @@ const GlassDashboard = ({ DashUser, setDashUser }) => {
           <li><a href='https://twitter.com/' target='blank'><i className="fa-brands fa-twitter"></i></a></li>
           <li><a href='https://www.instagram.com/?login&hl=en-gb' target='blank'><i className="fa-brands fa-instagram"></i></a></li>
         </ul>
-        <div className='copyrightText'>2023 All rights are reserved. Designed & developed by
-          <a href='https://github.com/ShamsQamar?tab=repositories' target='blank'> Shams</a>
+        <div className='copyrightText'>2025 © All rights are reserved. Designed & developed by
+          <a href='https://github.com/dashboard' target='blank'> Aravind Dumpoju S</a>
         </div>
 
 
